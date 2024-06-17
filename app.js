109e94706b77440e68ca98b79ca92324663528e9
Aze <102010085+NotStefaan@users.noreply.github.com>
2024-06-17 22:37:58 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function toggleItem(btn, itemId, price) {
 		btn.innerText = "Удалить из корзины";
 		let totalPrice = items.reduce((total, item) => total + item.price, 0);
 		if(totalPrice > 0){
-			tg.MainButton.setText('Общая цена товаров: ${totalPrice}');
+			tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
 			if(!tg.MainButton.isVisible){
 				tg.MainButton.show();
 			}
@@ -29,7 +29,7 @@ function toggleItem(btn, itemId, price) {
 		btn.innerText = "Добавить в корзину";
 		let totalPrice = items.reduce((total, item) => total + item.price, 0);
 		if (totalPrice > 0) {
-			tg.MainButton.setText('Общая цена товаров: ${totalPrice}');
+			tg.MainButton.setText(`Общая цена товаров: ${totalPrice}`);
 			if (!tg.MainButton.isVisible){
 				tg.MainButton.show();
 			}
